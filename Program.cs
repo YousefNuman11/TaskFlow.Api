d4c5cf67fa896b2b0922dd5146856c6127b85582
YousefNuman11 <yousefsnuman@gmail.com>
2026-02-22 01:18:57 +0300

    Add global exception handling middleware

--- Program.cs
+++ Program.cs
@@ -27,6 +27,20 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseExceptionHandler(errorApp =>
+{
+    errorApp.Run(async context =>
+    {
+        context.Response.StatusCode = 500;
+        context.Response.ContentType = "application/json";
+
+        await context.Response.WriteAsJsonAsync(new
+        {
+            message = "An unexpected error occurred."
+        });
+    });
+});
+
 app.MapTaskEndpoints();
 
 
